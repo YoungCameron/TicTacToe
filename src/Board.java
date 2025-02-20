ec7e95a8649795916ade73f90772bfcae75707b3
Cameron <46540209+YoungCameron@users.noreply.github.com>
2025-02-20 14:23:46 -0600

    UPD

--- src/Board.java
+++ src/Board.java
@@ -9,6 +9,16 @@ public class Board {
         this.boardSlots = new String[]{ "0", "1", "2" , "3", "4", "5" , "6" , "7", "8" };
     }
 
+    public void startGame() {
+        Scanner in = new Scanner(System.in);
+        printBoard();
+        do {
+            System.out.print("Player " + player + "\nEnter Slot Choice: ");
+            setSlot(Integer.parseInt(in.nextLine()));
+            printBoard();
+        } while (!gameWon() && !gameDraw());
+    }
+
     private void printBoard() {
         System.out.printf("""
                 %s | %s | %s
@@ -17,16 +27,6 @@ public class Board {
                 """, boardSlots[0], boardSlots[1], boardSlots[2],
                 boardSlots[3], boardSlots[4], boardSlots[5],
                 boardSlots[6], boardSlots[7], boardSlots[8]);
-    }
-
-    public void startGame() {
-        Scanner in = new Scanner(System.in);
-        printBoard();
-        do {
-            System.out.print("Player " + checkPlayer() + "\nEnter Slot Choice: ");
-            setSlot(in.nextInt());
-            printBoard();
-        } while (!gameWon() && !gameDraw());
     }
 
     private void setSlots(String pos, String setTo) {
@@ -39,11 +39,11 @@ public class Board {
 
     private void setSlot(int choice) {
             try {
-                if (checkPlayer().equals("p1")) {
+                if (player.equals("p1")) {
                     checkSlot(choice);
                     setSlots(choice + "", "X");
                     swapPlayer();
-                } else if (checkPlayer().equals("p2")) {
+                } else if (player.equals("p2")) {
                     checkSlot(choice);
                     setSlots(choice + "", "O");
                     swapPlayer();
@@ -71,17 +71,17 @@ public class Board {
 
     private boolean gameWon() {
         // Check rows
-        for (int i = 0; i < 3; i++) {
-            if (boardSlots[i * 3].equals(boardSlots[i * 3 + 1]) && boardSlots[i * 3 + 1].equals(boardSlots[i * 3 + 2])) {
-                System.out.println(boardSlots[i * 3] + "'s won");
+        for (int row = 0; row < 3; row++) {
+            if (boardSlots[row * 3].equals(boardSlots[row * 3 + 1]) && boardSlots[row * 3 + 1].equals(boardSlots[row * 3 + 2])) {
+                System.out.println(boardSlots[row * 3] + "'s won");
                 return true;
             }
         }
 
         // Check columns
-        for (int i = 0; i < 3; i++) {
-            if (boardSlots[i].equals(boardSlots[i + 3]) && boardSlots[i + 3].equals(boardSlots[i + 6])) {
-                System.out.println(boardSlots[i + 3] + "'s won");
+        for (int column = 0; column < 3; column++) {
+            if (boardSlots[column].equals(boardSlots[column + 3]) && boardSlots[column + 3].equals(boardSlots[column + 6])) {
+                System.out.println(boardSlots[column + 3] + "'s won");
                 return true;
             }
         }
@@ -99,13 +99,10 @@ public class Board {
         return false;
     }
 
-    private String checkPlayer() {
-        return this.player;
-    }
 
 
     private void swapPlayer() {
-        if (checkPlayer().equals("p1")) {
+        if (player.equals("p1")) {
             this.player = "p2";
         } else {
             this.player = "p1";
--- src/PlayGame.java
+++ src/PlayGame.java
@@ -1,6 +1,6 @@
 public class PlayGame {
     public static void main(String[] args) {
-        System.out.println("TicTacToe V1.0");
+        System.out.println("TicTacToe V1.0.0");
 
         Board gameBoard = new Board();
         gameBoard.startGame();
